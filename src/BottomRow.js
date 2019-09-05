357853d4fefaf7f891406a8f03e4ea71d92c6605
Kyle Richardson <kyle.richardson1@gmail.com>
2019-09-05 12:34:44 -0500

    cleanup code

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,54 +1,28 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 
   
-function BottomRow() {
-  const [quarter,setQuarter] = useState(1); 
-  const [down, setDown] = useState(1);
-  const [toGo, setToGo] = useState(10);
-  const [ballOn, setBallOn] = useState(20);
-  
-  const firstDown = e => {
-    setDown(1);
-    setToGo(10);
-  }
-  const moveBall = e => {
-    setBallOn(Math.floor(Math.random()*100)+1);
-  }
-  const nextQuarter = e => {
-    if (quarter == 4) setQuarter(0);
-    setQuarter(quarter+1);
-  }
-
-  useEffect(() => {
-    firstDown();
-  }, [down, toGo]);
-  useEffect(() => {
-    moveBall();
-  }, [ballOn]);
-  // useEffect(() => {
-  //   nextQuarter();
-  // }, [quarter]);
+function BottomRow(props) {
   
   
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">{down}</div>
+        <div className="down__value">{props.down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">{toGo}</div>
+        <div className="toGo__value">{props.toGo}</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">{ballOn}</div>
+        <div className="ballOn__value">{props.ballOn}</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">{quarter}</div>
+        <div className="quarter__value">{props.quarter}</div>
       </div>
     </div>
   );
